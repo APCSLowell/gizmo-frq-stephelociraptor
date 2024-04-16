f9178ccf1b119403ecb41077341ad4c475c2e42c
stephelociraptor <139821295+stephelociraptor@users.noreply.github.com>
2024-04-16 00:44:59 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -12,6 +12,14 @@ public class OnlinePurchaseManager
   public int countElectronicsByMaker(String maker)
   {
     /* to be implemented in part (a) */
+    int count = 0; 
+    for (int i = 0; i < purchases.size(); i++){
+      if (purchases.get(i).isElectronic() && purchases.get(i).getMaker().equals(maker)){
+        count++;
+      }
+    }
+    return count;
+    
   }
   
   /** Returns true if any pair of adjacent purchased Gizmo objects are
@@ -20,6 +28,12 @@ public class OnlinePurchaseManager
   public boolean hasAdjacentEqualPair()
   {
     /* to be implemented in part (b) */
+    for (int i = 0; i < purchases.size()-1; i+){
+      if (purchases.get(i).equals(purchases.get(i+1))){
+        return true;
+      }
+    }
+    return false; 
   }
   public OnlinePurchaseManager()
   {
